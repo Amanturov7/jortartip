bd90f3700981582b1cd53258c54cd260f70a0102
Amanturov7 <edi.amanturov@gmail.com>
2024-03-09 03:10:30 +0600

    tiles rebuild

--- src/main/java/kg/amanturov/jortartip/controller/TileController.java
+++ src/main/java/kg/amanturov/jortartip/controller/TileController.java
@@ -1,9 +1,5 @@
 package kg.amanturov.jortartip.controller;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,38 +7,45 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.io.InputStream;
-@RequestMapping("/rest/")
+import org.springframework.beans.factory.annotation.Value;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+@RequestMapping("/rest/")
 @RestController
 public class TileController {
 
     @Value("${tiles.storage}")
     private String tilesDirectory;
 
-    @GetMapping("/tiles/{z}/{x}/{y}.png")
-    public ResponseEntity<Resource> getTile(
+    @GetMapping("/{z}/{x}/{y}.png")
+    public ResponseEntity<byte[]> getTile(
             @PathVariable int z,
             @PathVariable int x,
             @PathVariable int y
     ) {
         String tilePath = String.format("%s/%d/%d/%d.png", tilesDirectory, z, x, y);
 
-        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(tilePath)) {
-            if (inputStream != null) {
-                byte[] tileBytes = inputStream.readAllBytes();
-                ByteArrayResource resource = new ByteArrayResource(tileBytes);
+        try (FileInputStream inputStream = new FileInputStream(new File(tilePath));
+             ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
 
-                return ResponseEntity.ok()
-                        .contentLength(tileBytes.length)
-                        .contentType(MediaType.IMAGE_PNG)
-                        .body(resource);
-            } else {
-                return ResponseEntity.notFound().build();
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
             }
+
+            byte[] tileBytes = outputStream.toByteArray();
+
+            return ResponseEntity.ok()
+                    .contentLength(tileBytes.length)
+                    .contentType(MediaType.IMAGE_PNG)
+                    .body(tileBytes);
         } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            return ResponseEntity.notFound().build();
         }
     }
 }
